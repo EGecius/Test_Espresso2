54757307caf85fb28539c37253a89a5435267ecd
update gradle <>
2017-12-15 12:54:39 +0000

    add MainActivityTest

---
+++ app/src/androidTest/java/com/egecius/test_espresso2/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.egecius.test_espresso2;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void textFound() {
+        onView(withText("from RealApiService")).check(matches(isDisplayed()));
+    }
+}
