a061477c0af96684e3102c6358ac72bb2ec0c2f5
update gradle <>
2017-12-27 12:21:40 +0000

    add detail activity

---
+++ app/src/main/java/com/egecius/test_espresso2/app/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.egecius.test_espresso2.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.egecius.test_espresso2.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
--- app/src/main/java/com/egecius/test_espresso2/app/MainActivity.java
+++ app/src/main/java/com/egecius/test_espresso2/app/MainActivity.java
@@ -1,13 +1,15 @@
 package com.egecius.test_espresso2.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-import com.egecius.test_espresso2.dependencies.ApiService;
 import com.egecius.test_espresso2.R;
+import com.egecius.test_espresso2.dependencies.ApiService;
 import com.egecius.test_espresso2.di.AppComponent;
 
 import javax.inject.Inject;
@@ -20,20 +22,35 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		injectDependencies();
-		setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        injectDependencies();
+		setListener();
         setText();
-    }
-
-    private void setText() {
-        TextView textView = (TextView) findViewById(R.id.view_to_set);
-        textView.setText(mApiService.getString());
     }
 
     private void injectDependencies() {
         RealApp application = (RealApp) getApplication();
         AppComponent appComponent = application.getComponent();
         appComponent.inject(this);
+    }
+
+    private void setListener() {
+        findViewById(R.id.to_detail_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDetailActivity();
+            }
+        });
+    }
+
+    private void showDetailActivity() {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
+
+    private void setText() {
+        TextView textView = (TextView) findViewById(R.id.view_to_set);
+        textView.setText(mApiService.getString());
     }
 
     @Override
