a078ff6faf748c26e6ef8bb19e8ed90a968673c9
update gradle <>
2017-12-28 12:16:54 +0000

    add mock module & component classes for subcomponent

--- app/src/main/java/com/egecius/test_espresso2/di/mock/MockAppComponent.java
+++ app/src/main/java/com/egecius/test_espresso2/di/mock/MockAppComponent.java
@@ -1,9 +1,13 @@
 package com.egecius.test_espresso2.di.mock;
 
 import com.egecius.test_espresso2.di.AppComponent;
+import com.egecius.test_espresso2.di.real.subcomponent.MockMySubcomponent;
+import com.egecius.test_espresso2.di.real.subcomponent.MockMySubcomponentModule;
 
 import dagger.Component;
 
 @Component (modules = MockAppModule.class)
 public interface MockAppComponent extends AppComponent {
+
+    MockMySubcomponent plus(MockMySubcomponentModule module);
 }
---
+++ app/src/main/java/com/egecius/test_espresso2/di/real/subcomponent/MockMySubcomponentModule.java
@@ -0,0 +1,18 @@
+package com.egecius.test_espresso2.di.real.subcomponent;
+
+import javax.inject.Named;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class MockMySubcomponentModule {
+
+    public static final String TAG = "MockMySubcomponentModule";
+
+    @Provides
+    @Named(TAG)
+    String providesString(){
+        return "from MockMySubcomponentModule";
+    }
+}
